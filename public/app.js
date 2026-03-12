99ba414fa2d61f9ec921f0081bcfa36789f9cd7b
svfcode <svfcode@mail.ru>
2026-03-12 10:57:19 +0300

    upd. code. change structure.

--- public/app.js
+++
@@ -1,6 +0,0 @@
-initModeSwitch();
-initBatchNav();
-
-document.getElementById('batchAccordionHeader').addEventListener('click', () => {
-  document.querySelector('.batch-accordion').classList.toggle('expanded');
-});
--- public/js/model.js
+++
@@ -1,3 +0,0 @@
-function createModel() {
-  return NN.create();
-}
